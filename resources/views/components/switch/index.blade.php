21d22bbc5251aad9483fd1d4ca08f786dd9fbe2d
Quentin Gabriele <quentin.gabriele@gmail.com>
2025-08-23 19:23:00 +0200

    fix switch

--- resources/views/components/switch/index.blade.php
+++ resources/views/components/switch/index.blade.php
@@ -4,7 +4,8 @@
     'id' => null,
 ])
 
-<label {!! $attributes->class(['flex items-start gap-2 cursor-pointer', $class]) !!} @style($style) @if ($id) for="{{ $id }}" @endif>
+<label @class(['flex items-start gap-2 cursor-pointer', $class]) @style($style)
+    @if ($id) for="{{ $id }}" @endif>
     <span class="relative">
         <x-kit::switch.base :attributes="$attributes" :id="$id" />
     </span>
