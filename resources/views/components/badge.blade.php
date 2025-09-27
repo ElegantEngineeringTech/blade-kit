650fdcd3fc4adb8cdcd42fcb170e5cfc9463564a
Quentin Gabriele <quentin.gabriele@gmail.com>
2025-09-27 20:37:07 +0200

    fix badge

--- resources/views/components/badge.blade.php
+++ resources/views/components/badge.blade.php
@@ -8,6 +8,6 @@
     'size-5' => $count > 1,
     'size-3' => $count <= 1,
     'el-badge el-text el-bg el-outline',
-]) !!}>
+]) !!} data-color="{{ $color }}">
     {{ $count > 1 ? min($count, $max) : null }}
 </span>
