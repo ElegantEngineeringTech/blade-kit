ab617cef42e2360b20332aebf985c7bbc5cf0534
Quentin Gabriele <quentin.gabriele@gmail.com>
2026-03-02 18:49:32 -0500

    clean

--- dist/components/ButtonBadge.vue.d.ts
+++
@@ -1,7 +0,0 @@
-type __VLS_Props = {
-    count?: number;
-};
-declare const _default: import('vue').DefineComponent<__VLS_Props, {}, {}, {}, {}, import('vue').ComponentOptionsMixin, import('vue').ComponentOptionsMixin, {}, string, import('vue').PublicProps, Readonly<__VLS_Props> & Readonly<{}>, {
-    count: number;
-}, {}, {}, {}, string, import('vue').ComponentProvideOptions, false, {}, HTMLSpanElement>;
-export default _default;
