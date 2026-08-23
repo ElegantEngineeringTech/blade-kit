e64ecbac38b554ff79a42b13099ccf7ec0e206ab
Quentin Gabriele <quentin.gabriele@gmail.com>
2026-08-23 23:06:08 +0200

    fix

--- resources/views/components/popover/content.blade.php
+++ resources/views/components/popover/content.blade.php
@@ -8,7 +8,7 @@
 ])
 
 <div {{ $attributes->class(['el-popover']) }} data-placement="{{ $placement }}" data-animation="{{ $animation }}"
-    role="{{ $role }}" x-cloak x-show="open" x-trap="trap && open" x-on:click.outside="hide" x-on:click.self="hide"
+    role="{{ $role }}" x-cloak x-show="open" x-trap="trap && open" x-on:click.outside="hide"
     x-on:keydown.escape.prevent="hide" x-anchor.{{ $placement }}.offset.{{ $offset }}="$refs.trigger">
     {{ $slot }}
 </div>
