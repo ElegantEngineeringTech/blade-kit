83502a3409b4af93833e388429712352496898e6
Quentin Gabriele <quentin.gabriele@gmail.com>
2026-08-24 22:20:39 +0200

    fix

--- resources/views/components/field/error.blade.php
+++ resources/views/components/field/error.blade.php
@@ -1,9 +1,7 @@
 @props(['error'])
 
-@if ($error)
-    @error($error)
-        <p {!! $attributes->class(['text-sm text-rose-500 font-semibold']) !!}>
-            {{ $message }}
-        </p>
-    @enderror
-@endif
+@error($error)
+    <p {!! $attributes->class(['text-sm text-rose-500 font-semibold']) !!}>
+        {!! $message !!}
+    </p>
+@enderror
